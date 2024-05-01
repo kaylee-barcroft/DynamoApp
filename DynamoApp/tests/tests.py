dbc2dae01ba0161ebfda004edc71824b534b7a18
kaylee-barcroft <kaylee.barcroft@gmail.com>
2024-05-01 11:55:03 -0600

    debugging testing

--- DynamoApp/tests/tests.py
+++ DynamoApp/tests/tests.py
@@ -9,9 +9,8 @@ class Hosttest(LiveServerTestCase):
     def testhomepage(self):
         driver = webdriver.Chrome()
         driver.get("http://127.0.0.1:8000/")
-        assert "Dynamo Coffee" in driver.title
-
         time.sleep(2)
+        assert "Dynamo Coffee" in driver.title
 
 
 class LoginFormTest(LiveServerTestCase):
@@ -34,62 +33,64 @@ class LoginFormTest(LiveServerTestCase):
 
         submit.send_keys(Keys.RETURN)
 
+        time.sleep(1)
+
         assert 'admin' in driver.page_source
 
 
-class SingleOriginFormTest(LiveServerTestCase):
+# class SingleOriginFormTest(LiveServerTestCase):
     
-    def test_create_form(self):
-        #login as admin
-        driver = webdriver.Chrome()
-        driver.get('http://127.0.0.1:8000/accounts/login/')
+#     def test_create_form(self):
+#         #login as admin
+#         driver = webdriver.Chrome()
+#         driver.get('http://127.0.0.1:8000/accounts/login/')
 
-        time.sleep(2)
+#         time.sleep(2)
 
-        user_name = driver.find_element(By.NAME, 'username')
-        user_password = driver.find_element(By.ID, 'id_password')
+#         user_name = driver.find_element(By.NAME, 'username')
+#         user_password = driver.find_element(By.ID, 'id_password')
 
-        time.sleep(2)
+        # time.sleep(2)
 
-        submit = driver.find_element(By.NAME, 'submit')
+        # submit = driver.find_element(By.NAME, 'submit')
 
-        user_name.send_keys('admin')
-        user_password.send_keys('admin')
+        # user_name.send_keys('admin')
+        # user_password.send_keys('admin')
 
-        submit.send_keys(Keys.RETURN)
+        # submit.send_keys(Keys.RETURN)
 
-        driver.get("http://127.0.0.1:8000/singleorigins/")
+        # driver.get("http://127.0.0.1:8000/singleorigins/")
 
-        add_offering = driver.find_element(By.NAME, 'add_offering')
-        add_offering.send_keys(Keys.RETURN)
+        # add_offering = driver.find_element(By.NAME, 'add_offering')
+        # add_offering.send_keys(Keys.RETURN)
 
-        time.sleep(3)
-        assert 'create' in driver.current_url
+        # time.sleep(2)
+        # assert 'create' in driver.current_url
 
-        farm_field = driver.find_element(By.NAME, 'farm')
-        about_field = driver.find_element(By.NAME, 'about')
-        roast_field = driver.find_element(By.NAME, 'roast_profile')
-        available_box = driver.find_element(By.NAME, 'available')
-        submit = driver.find_element(By.NAME, "submit")
+        # farm_field = driver.find_element(By.NAME, 'farm')
+        # about_field = driver.find_element(By.NAME, 'about')
+        # roast_field = driver.find_element(By.NAME, 'roast_profile')
+        # available_box = driver.find_element(By.NAME, 'available')
+        # submit = driver.find_element(By.NAME, "submit")
 
-        farm_field.send_keys("test field")
-        about_field.send_keys("This is a test field")
-        roast_field.send_keys("Berry")
-        available_box.send_keys(Keys.SPACE)
-        time.sleep(1)
-        submit.send_keys(Keys.RETURN)
-        time.sleep(2)
+        # farm_field.send_keys("test field")
+        # about_field.send_keys("This is a test field")
+        # roast_field.send_keys("Berry")
+        # available_box.send_keys(Keys.SPACE)
+        # time.sleep(1)
+        # submit.send_keys(Keys.RETURN)
+        
+        # time.sleep(2)
+        # #assert test field in db.sqlite3
+        # assert 'singleorigins' in driver.current_url # check if redirected to list view
 
-        #assert test field in db.sqlite3
-        assert 'singleorigins' in driver.current_url # check if redirected to list view
 
+# class SingleOriginDetailViewTest(LiveServerTestCase):
+#     def test_detail_view(self):
+#         driver = webdriver.Chrome()
+#         driver.get('http://127.0.0.1:8000/singleorigins/')
 
-class SingleOriginDetailViewTest(LiveServerTestCase):
-    def test_detail_view(self):
-        driver = webdriver.Chrome()
-        driver.get('http://127.0.0.1:8000/singleorigins/')
-
-        view_button = driver.find_element(By.LINK_TEXT, '/singleorigin/10/') # currently not working - need better search params
-        view_button.send_keys(Keys.RETURN)
-        time.sleep(2)
-        assert '/singleorigin/' in driver.current_url
+#         view_button = driver.find_element(By.LINK_TEXT, '/singleorigin/10/') # currently not working - need better search params
+#         view_button.send_keys(Keys.RETURN)
+#         time.sleep(2)
+#         assert '/singleorigin/' in driver.current_url
